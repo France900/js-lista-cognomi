a16f54b37de02e9a0b436bd31a53b9f6e9444f8b
France900 <francesco.em.livio@gmail.com>
2021-03-04 19:38:28 +0100

    finito

--- main.js
+++ main.js
@@ -8,6 +8,8 @@
 var promptInput = prompt("quale è il tuo cognome???")
 console.log("prompt", promptInput);
 
+promptInput = promptInput[0].toUpperCase() + promptInput.substring(1) // var = "stringa" --> var = ["s", "t", "r", "i", "n", "g", "a"]
+
 //creo array
 var archivio = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
 console.log("archivio", archivio);
@@ -23,9 +25,10 @@ for (var i = 0; i < sorted.length; i++) {
   console.log("sorted detro il for", sorted[i]);
 }
 
+document.getElementById("result").innerHTML += "<div>" + "Nella lista sei nella " + (sorted.indexOf(promptInput)+1) + " posizione." + "</div>"
+console.log("indexOf", sorted.indexOf(promptInput));
 
 // uso promptInput in un for confronto ogni valore e se è uguale lo stampo piu uno per il valore umano
-
-for (var i = 0; i < sorted.length; i++) {
-  sorted[i] == promptInput
-}
+// for (var i = 0; i < sorted.length; i++) {
+//   sorted[i] == promptInput
+// }
